5b758526f0baa9ffd9c32e89b62c7fe79643a09c
20tonik03 <is.2.karpushev@gmail.com>
2026-04-27 22:42:52 +0300

    Добавлены новые TODO

--- src/main/java/rsatu/ru/cw/Launcher.java
+++
@@ -1,9 +0,0 @@
-package rsatu.ru.cw;
-
-import javafx.application.Application;
-
-public class Launcher {
-    public static void main(String[] args) {
-        Application.launch(RentApplication.class, args);
-    }
-}
